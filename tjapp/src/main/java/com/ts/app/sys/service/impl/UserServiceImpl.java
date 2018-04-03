f1d1e6f76ba3b8696a13dddf2c253ccbfffb8eb1
15838028035 <15838028035@163.com>
2018-04-03 22:17:28 +0800

    commit files

--- tjapp/src/main/java/com/ts/app/sys/service/impl/UserServiceImpl.java
+++ tjapp/src/main/java/com/ts/app/sys/service/impl/UserServiceImpl.java
@@ -7,14 +7,11 @@ import org.springframework.stereotype.Service;
 
 import com.ts.app.sys.dao.UserMapper;
 import com.ts.app.sys.domain.User;
-import com.ts.app.sys.exception.CustomException;
 import com.ts.app.sys.service.UserService;
 
 /**
  * @ClassName: UserServiceImpl
  * @Description: TODO
- * @author: chunyu.xia
- * @date: 2017-11-15 下午1:52:56
  */
 @Service
 public class UserServiceImpl implements UserService {
@@ -22,15 +19,8 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	UserMapper userMapper;
 	
-	
-	
 	@Override
 	public List<User> findUserList(User user) {
 		return userMapper.findUserList(user);
 	}
-
-	
-
-	
-
 }
