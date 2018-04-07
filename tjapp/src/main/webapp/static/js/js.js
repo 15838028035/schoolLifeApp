367765a0cbc72da91d15a98e9137ac019971beaa
renwenhui <798844081@qq.com>
2018-04-07 12:16:13 +0800

    登陆

--- tjapp/src/main/webapp/static/js/js.js
+++ tjapp/src/main/webapp/static/js/js.js
@@ -1,3 +1,7 @@
+// 用于存放一些通用变量
+var ctx = window.location.protocol +"//"+ window.location.host;
+var ctxApp = window.location.protocol +"//"+ window.location.host+"/tsapp";
+
 // JavaScript Document
 $(function(){
 $('.forder li a').click(function(){	
@@ -5,14 +9,6 @@ $('.forder li a').click(function(){
 })
 $('.app_Guest li').click(function(){
 	$(this).addClass('avcet').siblings().removeClass('avcet')	
-})
-/*常见问题*/
-$('.problem').click(function(){
-	if($(this).hasClass('ancwer')){	
-		$(this).removeClass('ancwer')
-	}else{		
-		$(this).addClass('ancwer').siblings().removeClass('ancwer')
-	}	
 })
 
 $('.khlst').click(function(){
@@ -42,12 +38,6 @@ $('.Mask1').height(bodyheight)
 
 $('.web_sku').height(bodyheight)
 
-/*搜索选项卡*/
-
-$('.web_soso_tite li').click(function(){
-	$(this).addClass('liacor').siblings().removeClass('liacor')	
-})
-
 
 $('.mindor_3').click(function(){
 	$(this).css('background-image','url(img/sx_xz.png)')
@@ -63,9 +53,6 @@ $('.aist').click(function(){
 	
 })
 
-
-
-
 $('.hist').click(function(){
 	layer.open({
     content: '领取成功'
@@ -74,96 +61,14 @@ $('.hist').click(function(){
  });
 })
 
-
-
-$('.Coupon').click(function(){
- layer.open({
-    type: 1
-    ,className: 'amst'
-    ,content: $('#oimyt').html()
-    ,anim: 'up'
-    ,style: 'position:fixed; bottom:0; left:0; width: 100%; height: 100%; padding:10px; border:none; box-sizing: border-box; overflow: auto;'
-  });
-})
-
 $('body').on('click','.onctor_a',function(){
 	
 	layer.closeAll()
 })
 
-/*填写订单*/
-$('.asint').click(function(){
-      var exct = $('#exct').html()
-      var j = $(exct).clone();
-	  $('.Occupant_zont').append(j);	 
-})
+
 $('body').on('click','.remsint',function(){
 	$(this).parents('.Occupant').remove()
-})
-
-//填写订单,房屋加减
-
-$('.appjia').click(function() {
-	var pp = $('.appcont');
-	var getNum = parseInt(pp.html());
-	if(getNum < 1000000) {
-		pp.html(getNum + 1)
-	} else {
-		layer.open({		
-		  className:'sese',			
-          content: '不能大于99'		  
-          ,skin: 'msg'		  
-         ,time: 2 //2秒后自动关闭		 
-      });
-	}
-})
-$('.appjian').click(function() {
-	var pp = $('.appcont');
-	var getNum = parseInt(pp.html());
-	if(getNum > 1) {
-		pp.html(getNum - 1)
-	} else {
-		layer.open({		
-		  className:'sese',		
-          content: '不能小于1'	  
-          ,skin: 'msg'	  
-         ,time: 2 //2秒后自动关闭	 
-      });
-	}
-})	
-
-/*优惠券选择*/
-
-$('.inyent_left_inp').click(function(){
-	if($(this).hasClass('jop')){
-		$(this).removeClass('jop')
-	    $(this).find('input').prop('checked',false)
-	    $(this).css('background-image','url(img/xie@2x_32.png)')
-	}else{
-	$(this).addClass('jop').siblings().removeClass('jop')
-	$(this).find('input').prop('checked',true)
-	$(this).css('background-image','url(img/pj@2x_50.png)').siblings().css('background-image','url(img/xie@2x_32.png)')
-	}
-	
-})
-
-/*提交订单*/
-$('.forder_dist').click(function(){
-	
-	layer.open({		
-		  className:'sese',		
-          content: '提交成功'	  
-          ,skin: 'msg'	  
-         ,time: 2 //2秒后自动关闭	 
-      });
-	
-})
-
-/*优惠劵*/
-
-$('.Coupon_lin_tite li').click(function(){
-	$(this).addClass('acvio').siblings().removeClass('acvio')	
-	/*$('.attr_cont>.cenoct:eq(' + $(this).index() + ')').show().siblings().hide();*/
 })
 
 $('.biem_cont').click(function(){
